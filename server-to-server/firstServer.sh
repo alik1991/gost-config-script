e6ca5e97de3762fcb0f1b7ce8d43c77db4d50fe0
ali-k <katebi007@gmail.com>
2024-05-26 10:27:22 +0330

    refactor no.2 changes in where to store the script and automatically fill the ssh key in the destined position

--- server-to-server/firstServer.sh
+++ server-to-server/firstServer.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
+
 echo "Welcome to the Gost config script by acrux"
 echo ""
-echo "Step 1 create the key for ssh connection"
 
+# Ask user if they want to create a directory to store the script
+read -p "Do you want to create a directory to store your script? (yes/no): " CREATE_DIR
 
+if [[ $CREATE_DIR == "yes" ]]; then
+    read -p "Enter the path where you want to create the directory (default: /root/connections): " DIR_PATH
+    if [ -z "$DIR_PATH" ]; then
+        DIR_PATH="/root/connections"
+    fi
+    sudo mkdir -p "$DIR_PATH"
+    SCRIPT_PATH="$DIR_PATH"
+else
+    SCRIPT_PATH="/root"
+fi
+
+echo ""
+
+# Generate SSH key
+echo "Step 1: Create the key for SSH connection"
 read -p "Enter the name of the key: " KEY_NAME
 read -p "Enter the path to save the key (default: /root/.ssh): " KEY_PATH
 
@@ -11,63 +28,67 @@ if [ -z "$KEY_PATH" ]; then
   KEY_PATH="/root/.ssh"
 fi
 
-# Create the directory if it doesn't exist
 sudo mkdir -p "$KEY_PATH"
-
-# Generate the SSH key
 sudo ssh-keygen -t ed25519 -N "" -f "${KEY_PATH}/${KEY_NAME}"
 
 echo "SSH key generated at ${KEY_PATH}/${KEY_NAME}!"
 
-echo "Step 2 Sending The Public Key to the other server"
+# Get the public key path
+PB_KEY_PATH="${KEY_PATH}/${KEY_NAME}.pub"
 
+echo ""
 
 # Send Public Key to the Other Server
+echo "Step 2: Sending The Public Key to the other server"
 
-read -p "Enter the destination port (if the port is the default please enter 22): " DS_PORT
-read -p "Enter the Public key path that you want transfer: " PB_KEY_PATH
+read -p "Enter the destination port (default: 22): " DS_PORT
 read -p "Enter the USERNAME for destination ip address: " USR
 read -p "Enter the destination IP address: " DS_IP
 read -p "Enter the destination key path that you want to transfer: " DS_PATH
 
-
 scp -P ${DS_PORT} ${PB_KEY_PATH} ${USR}@${DS_IP}:${DS_PATH}
 
-echo "Public Key Transfered"
+echo "Public Key Transferred"
 
-echo "Create The supervisor config for your bash script"
-# Prompt for config name and program name
+echo ""
+
+# Create Gost config
+echo "Step 3: Create the Gost config"
 read -p "Enter the name of the connection: " CONF_NAME
+read -p "Enter the ServerSide Address: " IP_ADDRESS
+read -p "Enter the ServerSide port: " SR_PORT
+read -p "Enter the desired port that you want to connect: " CL_PORT
+read -p "Please provide your own password (note that the password should be 12 characters): " PASSWORD
+
+# Get the private key path
+PR_KEY_PATH="${KEY_PATH}/${KEY_NAME}"
+
+cat> ${SCRIPT_PATH}/${CONF_NAME}.sh <<EOF
+#!/bin/bash
+gost -L=ss+ohttp://chacha20-ietf-poly1305:${PASSWORD}@:${CL_PORT} -F="ssh://${IP_ADDRESS}:${SR_PORT}?ssh_key=${PR_KEY_PATH}" 
+EOF
+
+sudo chmod +x ${SCRIPT_PATH}/${CONF_NAME}.sh
+
+echo "Gost config created at ${SCRIPT_PATH}/${CONF_NAME}.sh"
+
+echo ""
 
 # Write supervisor config file
+echo "Step 4: Create the Supervisor config for your bash script"
 cat> /etc/supervisor/conf.d/${CONF_NAME}.conf <<EOF
 [program:${CONF_NAME}]
-command=/root/${CONF_NAME}.sh
+command=${SCRIPT_PATH}/${CONF_NAME}.sh
 autostart=true
 autorestart=true
 stderr_logfile=/var/log/${CONF_NAME}.err.log
 stdout_logfile=/var/log/${CONF_NAME}.out.log
 EOF
 
-#Write gost config for client side
-read -p "Enter the ServerSide Address: " IP_ADDRESS
-read -p "Enter the ServerSide port: " SR_PORT
-read -p "Enter the Private Key Path: " PR_KEY_PATH
-read -p "Enter the your desired port that you want to connect: " CL_PORT
-read -p "Please provide your own password(note that the password should be 12 character): " PASSWORD
-
-#Create the gost config
-cat> /root/${CONF_NAME}.sh <<EOF
-#!/bin/bash
-gost -L=ss+ohttp://chacha20-ietf-poly1305:${PASSWORD}@:${CL_PORT} -F="ssh://${IP_ADDRESS}:${SR_PORT}?ssh_key=${PR_KEY_PATH}" 
-EOF
-
-#Make the Programm executable
-sudo chmod +x /root/${CONF_NAME}.sh
-
 # Restart supervisor to apply changes
 sudo supervisorctl reread
 sudo supervisorctl update
 
+echo ""
 
 echo "Gost Configured, Enjoy"
